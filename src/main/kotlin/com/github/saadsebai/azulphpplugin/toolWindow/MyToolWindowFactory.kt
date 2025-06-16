858a312a5186f1dc614c50ce8306dcc71dd6a06c
GitHub Action <action@github.com>
2025-06-16 16:52:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/saadsebai/azulphpplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/saadsebai/azulphpplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.saadsebai.azulphpplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.saadsebai.azulphpplugin.MyBundle
+import com.github.saadsebai.azulphpplugin.services.MyProjectService
 import javax.swing.JButton
 
 
